20318bd04a67b3a6cb1f1e8747eb45a299c40098
Hasan Sattar <hasanraza1798@gmail.com>
2023-06-24 22:45:42 +0100

    added index file which imports and exports all the functions

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { binarySearch } from './algorithms/binary-search'
+import { bubbleSort } from './algorithms/bubble-sort'
+import { insertionSort } from './algorithms/insertion-sort'
+import { linearSearch } from './algorithms/linear-search'
+import { mergeSort } from './algorithms/merge-sort'
+import { selectionSort } from './algorithms/selection-sort'
+
+import { createStack } from './data-structures/stack'
+import { createQueue } from './data-structures/queue'
+
+export default { binarySearch, bubbleSort, insertionSort, linearSearch, mergeSort, selectionSort, createStack, createQueue }
