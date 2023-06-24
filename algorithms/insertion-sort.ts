b4558391341313a11f40fef4e88b4088cdeea205
Hasan Sattar <hasanraza1798@gmail.com>
2023-06-24 22:40:32 +0100

    completed insertion sort function

--- algorithms/insertion-sort.ts
+++ algorithms/insertion-sort.ts
@@ -1,9 +1,26 @@
 export const insertionSort = (array: number[]): number[] => {
     
-    const sortedArray: number[] = []
+    const sortedArray: number[] = [array[0]]
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
+        
+        let indexToInsert = i + 1
 
+        for (let j = sortedArray.length - 1; j >= 0; j--) {
+
+            indexToInsert--
+
+            if (array[i] < sortedArray[j]) {
+                if (j === 0) {
+                    sortedArray.unshift(array[i])
+                }
+            } else {
+                sortedArray.splice(indexToInsert, 0, array[i])
+                break
+            }
+
+        }
+        
     }
 
     return sortedArray
