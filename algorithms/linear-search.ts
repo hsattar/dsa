5ab32eaa37f91cde91faf3ed6feef68576aa7e00
Hasan Sattar <hasanraza1798@gmail.com>
2023-06-24 16:46:56 +0100

    wrote logic for linear search

--- algorithms/linear-search.ts
+++ algorithms/linear-search.ts
@@ -1,6 +1,15 @@
 export const linearSearch = (array: number[], searchValue: number): IReturnValue => {
     let matchFound = false
     let index = -1
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === searchValue) {
+            matchFound = true
+            index = i
+            break
+        }
+    }
+
     return {  matchFound, index }
 }
 
