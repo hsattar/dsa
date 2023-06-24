f9d90313cb9e8f6f565850b443eb1dafa774adce
Hasan Sattar <hasanraza1798@gmail.com>
2023-06-24 15:36:41 +0100

    started to work on bubble sort

--- algorithms/bubble-sort.ts
+++ algorithms/bubble-sort.ts
@@ -1,3 +1,11 @@
 export const bubbleSort = (array: number[]): number[] => {
-    return array
+    
+    const sortedArray: number[] = []
+
+    for (let i = 0; i < array.length; i++) {
+        const rightNumber = array[array.length - 1]
+        const leftNumber = array[array.length - 2]
+    }
+
+    return sortedArray
 }
