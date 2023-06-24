93adac21c15b691a0cd978f63a5015a7a358f188
Hasan Sattar <hasanraza1798@gmail.com>
2023-06-24 21:47:28 +0100

    added binary search function and changed test to return boolean if value is found

--- __tests__/binary-search.test.ts
+++ __tests__/binary-search.test.ts
@@ -4,12 +4,11 @@ import { binarySearch } from "../algorithms/binary-search"
 
 describe('Linear Search', () => {
 
-    it('SHOULD RETURN AN OBJECT WITH A MATCH FOUND AND INDEX PROPERTY', () => {
+    it('SHOULD RETURN A BOOLEAN', () => {
         const input = [1, 2, 3, 4, 5]
         const searchValue = 5
         const result = binarySearch(input, searchValue)
-        expect(result).toHaveProperty('matchFound', expect.any(Boolean))
-        expect(result).toHaveProperty('index', expect.any(Number))
+        expect(typeof result).toBe('boolean')
     })
 
     it('SHOULD NOT MODIFY THE ORIGINAL INPUT', () => {
@@ -20,34 +19,34 @@ describe('Linear Search', () => {
       expect(input).toEqual(inputCopy)
     })
 
-    it('SHOULD RETURN AN OBJECT WITH THE MATCH FOUND PROPERTY SET TO FALSE AND THE INDEX SET TO -1 IT CANT FIND THE SEARCH VALUE', () => {
+    it('SHOULD RETURN FALSE IF IT CANT FIND THE SEARCH VALUE', () => {
         const input = [1, 2, 3, 4]
         const searchValue = 9
-        const expectedOutput = { matchFound: false, index: -1 }
+        const expectedOutput = false
         const result = binarySearch(input, searchValue)
         expect(result).toEqual(expectedOutput)
     })
 
-    it('SHOULD RETURN AN OBJECT WITH THE MATCH FOUND PROPERTY SET TO TRUE AND THE INDEX OF THE VALUE FOUND IF IT FINDS THE SEARCH VALUE IN A SMALL ARRAY', () => {
+    it('SHOULD RETURN TRUE IF IT FINDS THE SEARCH VALUE IN A SMALL ARRAY', () => {
         const input = [1, 2, 3, 4, 5]
         const searchValue = 5
-        const expectedOutput = { matchFound: true, index: 4 }
+        const expectedOutput = true
         const result = binarySearch(input, searchValue)
         expect(result).toEqual(expectedOutput)
     })
 
-    it('SHOULD RETURN AN OBJECT WITH THE MATCH FOUND PROPERTY SET TO TRUE AND THE INDEX OF THE VALUE FOUND IF IT FINDS THE SEARCH VALUE IN A SLIGHTLY LARGER ARRAY', () => {
+    it('SHOULD RETURN TRUEIF IT FINDS THE SEARCH VALUE IN A SLIGHTLY LARGER ARRAY', () => {
         const input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         const searchValue = 3
-        const expectedOutput = { matchFound: true, index: 2 }
+        const expectedOutput = true
         const result = binarySearch(input, searchValue)
         expect(result).toEqual(expectedOutput)
     })
 
-    it('SHOULD RETURN AN OBJECT WITH THE MATCH FOUND PROPERTY SET TO TRUE AND THE INDEX OF THE VALUE FOUND IF IT FINDS THE SEARCH VALUE IN A LARGE ARRAY', () => {
+    it('SHOULD RETURN TRUE IF IT FINDS THE SEARCH VALUE IN A LARGE ARRAY', () => {
         const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
         const searchValue = 15
-        const expectedOutput = { matchFound: true, index: 14 }
+        const expectedOutput = true
         const result = binarySearch(input, searchValue)
         expect(result).toEqual(expectedOutput)
     })
--- algorithms/binary-search.ts
+++ algorithms/binary-search.ts
@@ -1,10 +1,19 @@
-export const binarySearch = (array: number[], searchValue: number): IReturnValue => {
+export const binarySearch = (array: number[], searchValue: number): boolean => {
+    
+    if (array.length === 0) return false
+    
     let matchFound = false
-    let index = -1
-    return {  matchFound, index }
-}
 
-interface IReturnValue {
-    matchFound: boolean
-    index: number
+    const middleIndex = array.length % 2 === 0 ? array.length / 2 :  Math.floor(array.length / 2)
+    const middleValue = array[middleIndex]
+
+    if (middleValue === searchValue) {
+        return true
+    } else if (middleValue < searchValue) {
+        return binarySearch(array.slice(middleIndex + 1, array.length), searchValue)
+    } else if (middleValue > searchValue) {
+        return binarySearch(array.slice(0, middleIndex), searchValue)
+    }
+
+    return matchFound
 }
